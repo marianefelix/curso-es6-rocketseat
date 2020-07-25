b783727b0fd2661ddaa6bb918e035a4a5e27efdf
marianefelix <marianefelix59@gmail.com>
2020-07-25 13:58:03 -0300

    Módulo 3

---
+++ modulo3/async-await.js
@@ -0,0 +1,12 @@
+const minhaPromise = () => new Promise((resolve, reject) => {
+    setTimeout(() => { resolve('OK') }, 2000);
+
+});
+
+const executaPromise = async () => {
+    console.log(await minhaPromise());
+    console.log(await minhaPromise());
+    console.log(await minhaPromise());
+};
+
+executaPromise();
